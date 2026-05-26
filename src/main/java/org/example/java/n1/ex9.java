a08361abb2feed50f9268c5f645289e7cb625e66
Gabriel Potje <gpotje.souza@gmail.com>
2026-05-26 15:29:02 -0300

    exercicio 9 parcialmente

---
+++ src/main/java/org/example/java/n1/ex9.java
@@ -0,0 +1,56 @@
+package org.example.java.n1;
+
+public class ex9 {
+    public static void main(String[] args) {
+
+        int numbers[] = {1, 0, 2, 0, 3, 0, 4}; // test 1°
+        int countZero = 0;
+        int countNumeber = 0;
+        int arrayZero[];
+        int arraynumbers[];
+
+
+        for(int i = 0; i < numbers.length; i++){
+            if(numbers[i] == 0){
+                countZero ++;
+            }else{
+                countNumeber ++;
+            }
+        }
+
+        arrayZero = new int[countZero];
+        arraynumbers = new int[countNumeber];
+        countNumeber = 0;
+        countZero = 0;
+
+        for(int i = 0; i < numbers.length; i++){
+            if(numbers[i] == 0){
+                arrayZero[countZero] = numbers[i];
+                countZero ++;
+            }else{
+                arraynumbers[countNumeber] = numbers[i];
+                countNumeber ++;
+            }
+        }
+
+        int arryFinal = 0;
+
+        for(int i = 0; i < arraynumbers.length; i++){
+            //System.out.print(arraynumbers[i]+", ");
+            numbers[arryFinal] = arraynumbers[i];
+            arryFinal++;
+        }
+        for(int i = 0; i < arrayZero.length; i++){
+            //System.out.print(arrayZero[i]+", ");
+            numbers[arryFinal] = arrayZero[i];
+            arryFinal++;
+        }
+
+        System.out.println("============ Array Final ============");
+        for(int i = 0; i < numbers.length; i++){
+            System.out.print(numbers[i]+", ");
+        }
+
+
+    }
+}
